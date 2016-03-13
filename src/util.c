e92c5173e3bdf31a44d74e66bad1725b9c27a62f
Vegard Sandengen <vegardsandengen@gmail.com>
2016-03-13 16:46:10 +0100

    style: formatting

--- src/util.c
+++ src/util.c
@@ -10,20 +10,21 @@
 
 //! PUBLIC API
 char *
-dx_semver_string(char *buffer, int32_t buffer_size, struct semantic_version *semver)
+dx_semver_string (char *buffer, int32_t buffer_size, struct semantic_version *semver)
 {
     int res;
     if (buffer == NULL || semver == NULL)
     {
-        log_warn("%s invoked with NULL buffer or semverpointer", __FUNCTION__);
+        log_warn ("%s invoked with NULL buffer or semverpointer", __FUNCTION__);
         return "<invalid_arguments>";
     }
 
-    res = snprintf(buffer, buffer_size, "%d.%d.%d.", semver->sv_major, semver->sv_minor, semver->sv_patch);
+    res = snprintf (buffer, buffer_size,
+            "%d.%d.%d.", semver->sv_major, semver->sv_minor, semver->sv_patch);
     if (res < 0 || res >= buffer_size)
     {
-        log_warn("Insufficient buffer( %p ) size( %d ) to copy semver (%d.%d.%d) - Res: %d", 
-                    buffer, buffer_size, semver->sv_major, semver->sv_minor, 
+        log_warn ("Insufficient buffer( %p ) size( %d ) to copy semver (%d.%d.%d) - Res: %d",
+                    buffer, buffer_size, semver->sv_major, semver->sv_minor,
                     semver->sv_patch, res);
         return "<insufficient_buffer>";
     }
@@ -33,7 +34,7 @@ dx_semver_string(char *buffer, int32_t buffer_size, struct semantic_version *sem
 
 //! PUBLIC API
 int
-dx_semantic_version_compare(struct semantic_version *s1, struct semantic_version *s2)
+dx_semantic_version_compare (struct semantic_version *s1, struct semantic_version *s2)
 {
     int res;
 
