68d7e8733aeb9f76139c5f46bd8cf9de70bb0b8d
Vegard Sandengen <vegardsandengen@gmail.com>
2016-03-14 20:05:16 +0100

    style: context_documentation.c

--- src/context_documentation.c
+++ src/context_documentation.c
@@ -17,7 +17,7 @@
 
 //! INTERNAL API
 struct disir_documentation *
-dx_documentation_fetch(dc_t *context)
+dx_documentation_fetch (dc_t *context)
 {
     struct disir_documentation *doc;
 
@@ -35,7 +35,7 @@ dx_documentation_fetch(dc_t *context)
     case DISIR_CONTEXT_GROUP:
     case DISIR_CONTEXT_KEYVAL:
     {
-        dx_crash_and_burn("unhandled not implemented");
+        dx_crash_and_burn ("unhandled not implemented");
         break;
     }
     case DISIR_CONTEXT_TYPE:
@@ -55,7 +55,7 @@ dx_documentation_fetch(dc_t *context)
 
 //! INTERNAL API
 enum disir_status
-dx_documentation_add(dc_t *parent, struct disir_documentation *doc)
+dx_documentation_add (dc_t *parent, struct disir_documentation *doc)
 {
     struct disir_documentation **parent_doc;
     struct disir_documentation *tmp;
@@ -69,9 +69,9 @@ dx_documentation_add(dc_t *parent, struct disir_documentation *doc)
     if (parent == NULL || doc == NULL)
         return DISIR_STATUS_INVALID_ARGUMENT;
 
-    log_info_context(parent, "Adding documentation entry to context,");
+    log_info_context (parent, "Adding documentation entry to context,");
 
-    switch(dc_type(parent))
+    switch(dc_type (parent))
     {
     case DISIR_CONTEXT_CONFIG:
     {
@@ -83,7 +83,7 @@ dx_documentation_add(dc_t *parent, struct disir_documentation *doc)
     case DISIR_CONTEXT_GROUP:
     case DISIR_CONTEXT_KEYVAL:
     {
-        dx_crash_and_burn("unhandled not implemented");
+        dx_crash_and_burn ("unhandled not implemented");
         break;
     }
     case DISIR_CONTEXT_TYPE:
@@ -98,30 +98,30 @@ dx_documentation_add(dc_t *parent, struct disir_documentation *doc)
     // No default - let compiler  detect unhandled type
     }
 
-    // Function invoked on wrong context. 
-    // Programming error - 
+    // Function invoked on wrong context.
+    // Programming error
     if (parent_doc == NULL)
     {
         // LOGWARN
-        log_debug_context(parent, "parent_doc is NULL - we cannot add doc entry");
+        log_debug_context (parent, "parent_doc is NULL - we cannot add doc entry");
         status = DISIR_STATUS_WRONG_CONTEXT;
     }
     // Add to parent storage if no entry exists
     else if (*parent_doc == NULL)
     {
-        log_debug_context(parent, "doesn't contain any doc entries. Adding as only element.");
+        log_debug_context (parent, "doesn't contain any doc entries. Adding as only element.");
         *parent_doc = doc;
         status = DISIR_STATUS_OK;
     }
     // There exists multiple entries - insert into storage
     else
     {
-        log_debug_context(parent, "contains documentation entires. Adding in-place");
+        log_debug_context (parent, "contains documentation entires. Adding in-place");
         // Assume everything is okay
         status = DISIR_STATUS_OK;
 
         // Special first case - need to change the parent_doc pointer.
-        res = dx_semantic_version_compare(&((*parent_doc)->dd_introduced), &(doc->dd_introduced));
+        res = dx_semantic_version_compare (&((*parent_doc)->dd_introduced), &(doc->dd_introduced));
         if (res > 0)
         {
             doc->dd_next = *parent_doc;
@@ -166,7 +166,7 @@ dx_documentation_add(dc_t *parent, struct disir_documentation *doc)
                 }
 
                 tmp = tmp->dd_next;
-                res = dx_semantic_version_compare(&tmp->dd_introduced, &doc->dd_introduced);
+                res = dx_semantic_version_compare (&tmp->dd_introduced, &doc->dd_introduced);
             }
         }
     }
@@ -175,7 +175,7 @@ dx_documentation_add(dc_t *parent, struct disir_documentation *doc)
 }
 
 uint32_t
-dx_documentation_exists(dc_t *context)
+dx_documentation_exists (dc_t *context)
 {
     struct disir_documentation *doc;
 
@@ -187,13 +187,13 @@ dx_documentation_exists(dc_t *context)
 
 //! PUBLIC API
 enum disir_status
-dc_add_documentation(dc_t *parent, char *doc, int32_t doc_size)
+dc_add_documentation (dc_t *parent, char *doc, int32_t doc_size)
 {
     dc_t *context;
     enum disir_status status;
 
     // Check arguments
-    status = CONTEXT_NULL_INVALID_TYPE_CHECK(parent);
+    status = CONTEXT_NULL_INVALID_TYPE_CHECK (parent);
     if (status != DISIR_STATUS_OK)
     {
         // Already logged.
@@ -201,44 +201,44 @@ dc_add_documentation(dc_t *parent, char *doc, int32_t doc_size)
     }
     if (doc == NULL || doc_size <= 0)
     {
-        dx_log_context(parent, "Documentation string must be non-null, of possitive length.");
-        log_debug("(doc: %p\tdoc_size: %d)", doc, doc_size);
+        dx_log_context (parent, "Documentation string must be non-null, of possitive length.");
+        log_debug ("(doc: %p\tdoc_size: %d)", doc, doc_size);
         return DISIR_STATUS_INVALID_ARGUMENT;
     }
 
     // Construct a simple document context with default values.
     // The SemVer version will be default.
     // Any invalid state building operation is reported to parent context
-    log_debug("Arguments ok - begining doc");
+    log_debug ("Arguments ok - begining doc");
 
-    status = dx_documentation_begin(parent, &context);
+    status = dx_documentation_begin (parent, &context);
     if (status != DISIR_STATUS_OK)
     {
         // dc_documentation_begin performs LOGWARN
         return status;
     }
 
-    status = dc_add_value_string(context, doc, doc_size);
+    status = dc_add_value_string (context, doc, doc_size);
     if (status != DISIR_STATUS_OK)
     {
         // dc_add_value_string performs LOGWARN
-        dx_context_transfer_logwarn(parent, context);
-        dc_destroy(&context);
+        dx_context_transfer_logwarn (parent, context);
+        dc_destroy (&context);
         return status;
     }
 
-    status = dx_documentation_finalize(&context);
+    status = dx_documentation_finalize (&context);
     if (status != DISIR_STATUS_OK)
     {
-        dx_context_transfer_logwarn(parent, context);
-        dc_destroy(&context);
+        dx_context_transfer_logwarn (parent, context);
+        dc_destroy (&context);
     }
     return status;
 }
 
 //! INTERNAL API
 enum disir_status
-dx_documentation_begin(dc_t *parent, dc_t **doc)
+dx_documentation_begin (dc_t *parent, dc_t **doc)
 {
     dc_t *context;
 
@@ -246,7 +246,8 @@ dx_documentation_begin(dc_t *parent, dc_t **doc)
     if (parent == NULL || doc == NULL)
     {
         // LOGWARN
-        log_warn("%s: parent and/or doc invoked with NULL pointers. (parent: %p, doc: %p)", __FUNCTION__, parent, doc);
+        log_warn ("%s: parent and/or doc invoked with NULL pointers. (parent: %p, doc: %p)",
+                __FUNCTION__, parent, doc);
         return DISIR_STATUS_INVALID_ARGUMENT;
     }
 
@@ -254,46 +255,48 @@ dx_documentation_begin(dc_t *parent, dc_t **doc)
     if ((parent->cx_capabilities & CC_ADD_DOCUMENTATION) == 0)
     {
         // LOGWARN
-        dx_log_context(parent, "No capability: %s", dx_context_capability_string(CC_ADD_DOCUMENTATION));
+        dx_log_context (parent, "No capability: %s",
+                dx_context_capability_string (CC_ADD_DOCUMENTATION));
         return DISIR_STATUS_NO_CAN_DO;
     }
 
-    log_debug_context(parent, "capable of adding single documentataion.");
+    log_debug_context (parent, "capable of adding single documentataion.");
 
     // Check if we can add multiple documentation entries
     if (dx_documentation_exists(parent))
     {
-        log_debug("documentation exists");
+        log_debug ("documentation exists");
         if ((parent->cx_capabilities & CC_ADD_MULTIPLE_DOCUMENTATION) == 0)
         {
             // LOGWARN
-            dx_log_context(parent, "Contains one documentation entry. It has no capability: %s", dx_context_capability_string(CC_ADD_MULTIPLE_DOCUMENTATION));
+            dx_log_context (parent, "Contains one documentation entry. It has no capability: %s",
+                    dx_context_capability_string (CC_ADD_MULTIPLE_DOCUMENTATION));
             return DISIR_STATUS_EXISTS;
         }
     }
 
-    log_debug_context(parent, "capable of adding documentation context");
+    log_debug_context (parent, "capable of adding documentation context");
 
-    context = dx_context_create(DISIR_CONTEXT_DOCUMENTATION);
+    context = dx_context_create (DISIR_CONTEXT_DOCUMENTATION);
     if (context == NULL)
     {
         // LOGWARN
-        log_debug_context(parent, "failed to allocate new document context");
+        log_debug_context (parent, "failed to allocate new document context");
         return DISIR_STATUS_NO_MEMORY;
     }
 
-    log_debug_context(parent, "created context: %p", context);
+    log_debug_context (parent, "created context: %p", context);
 
     context->cx_documentation = dx_documentation_create(context);
     if (context->cx_documentation == NULL)
     {
         // LOGWARN
-        dx_context_destroy(&context);
-        dx_log_context(parent, "cannot allocate new document instance");
+        dx_context_destroy (&context);
+        dx_log_context (parent, "cannot allocate new document instance");
         return DISIR_STATUS_NO_MEMORY;
     }
 
-    log_debug_context(parent, "allocated documentation instance: %p", context->cx_documentation);
+    log_debug_context (parent, "allocated documentation instance: %p", context->cx_documentation);
 
     dx_context_attach(parent, context);
     *doc = context;
@@ -302,7 +305,7 @@ dx_documentation_begin(dc_t *parent, dc_t **doc)
 
 //! INTERNAL API
 enum disir_status
-dx_documentation_finalize(dc_t **doc)
+dx_documentation_finalize (dc_t **doc)
 {
     enum disir_status status;
 
@@ -312,11 +315,11 @@ dx_documentation_finalize(dc_t **doc)
     if (doc == NULL || *doc == NULL)
     {
         // LOGWARN
-        log_debug("invoked with null parameters");
+        log_debug ("invoked with null parameters");
         return DISIR_STATUS_INVALID_ARGUMENT;
     }
 
-    status = dx_documentation_add((*doc)->cx_parent_context, (*doc)->cx_documentation);
+    status = dx_documentation_add ((*doc)->cx_parent_context, (*doc)->cx_documentation);
 
     if (status == DISIR_STATUS_OK)
     {
@@ -329,12 +332,14 @@ dx_documentation_finalize(dc_t **doc)
 
 //! INTERNAL API
 enum disir_status
-dx_documentation_add_value_string(struct disir_documentation *doc, const char *value, int32_t value_size)
+dx_documentation_add_value_string (struct disir_documentation *doc,
+                                   const char *value,
+                                   int32_t value_size)
 {
     // At this point, simply allocate /reallocate and override exiting shizzle
 
     if (doc->dd_string == NULL || doc->dd_string_allocated - 1 < value_size)
-    {   
+    {
         // Just free the existing memory. We allocate a larger one below
         if (doc->dd_string_allocated - 1 < value_size )
         {
@@ -345,11 +350,11 @@ dx_documentation_add_value_string(struct disir_documentation *doc, const char *v
         if (doc->dd_string == NULL)
         {
             // Size of requested string + 1 for NULL terminator
-            doc->dd_string = calloc(1, value_size + 1);
+            doc->dd_string = calloc (1, value_size + 1);
             if (doc->dd_string == NULL)
             {
                 // LOGWARN
-                fprintf(stderr, "Failed to allocate memory for doc string");
+                fprintf (stderr, "Failed to allocate memory for doc string");
                 return DISIR_STATUS_NO_MEMORY;
             }
             doc->dd_string_allocated = value_size;
@@ -357,7 +362,7 @@ dx_documentation_add_value_string(struct disir_documentation *doc, const char *v
     }
 
     // Copy the incoming docstring to freely available space
-    memcpy(doc->dd_string, value, value_size);
+    memcpy (doc->dd_string, value, value_size);
     doc->dd_string_size = value_size;
 
     // Terminate it with a zero terminator. Just to be safe.
@@ -368,11 +373,11 @@ dx_documentation_add_value_string(struct disir_documentation *doc, const char *v
 
 //! INTERNAL API
 struct disir_documentation *
-dx_documentation_create(dc_t *context)
+dx_documentation_create (dc_t *context)
 {
     struct disir_documentation *doc;
 
-    doc = calloc(1, sizeof(struct disir_documentation));
+    doc = calloc(1, sizeof (struct disir_documentation));
     if (doc == NULL)
         return NULL;
 
@@ -383,7 +388,7 @@ dx_documentation_create(dc_t *context)
 
 //! INTERNAL API
 enum disir_status
-dx_documentation_destroy(struct disir_documentation **documentation)
+dx_documentation_destroy (struct disir_documentation **documentation)
 {
     struct disir_documentation *tmp;
 
