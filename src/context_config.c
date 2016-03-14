846a1bdeeed63d397e1c595f70d8061af257619d
Vegard Sandengen <vegardsandengen@gmail.com>
2016-03-14 20:07:33 +0100

    style: context_config.c

--- src/context_config.c
+++ src/context_config.c
@@ -13,24 +13,24 @@
 
 //! PUBLIC API
 dc_t *
-dc_config_getcontext(struct disir_config *config)
+dc_config_getcontext (struct disir_config *config)
 {
     // Check arguments
     if (config == NULL)
     {
         // LOGWARN
-        log_debug("invoked with NULL pointer");
+        log_debug ("invoked with NULL pointer");
         return NULL;
     }
 
-    dx_context_incref(config->cf_context);
+    dx_context_incref (config->cf_context);
 
     return config->cf_context;
 }
 
 //! PUBLIC API
 enum disir_status
-dc_config_begin(dc_t **config)
+dc_config_begin (dc_t **config)
 {
     dc_t *context;
 
@@ -40,25 +40,25 @@ dc_config_begin(dc_t **config)
     if (config == NULL)
     {
         // LOGWARN
-        log_debug("invoked with NULL config pointer");
+        log_debug ("invoked with NULL config pointer");
         return DISIR_STATUS_INVALID_ARGUMENT;
     }
 
     // Allocate a context to be returned regardless of outcome.
-    context = dx_context_create(DISIR_CONTEXT_CONFIG);
+    context = dx_context_create (DISIR_CONTEXT_CONFIG);
     if (context == NULL)
     {
         // LOGWARN
-        log_error("failed to allocate context for config");
+        log_error ("failed to allocate context for config");
         return DISIR_STATUS_NO_MEMORY;
     }
 
     // Allocate a disir_config for this context.
-    context->cx_config = dx_config_create(context);
+    context->cx_config = dx_config_create (context);
     if (context->cx_config == NULL)
     {
-        log_error("failed to allocate config for context");
-        dx_context_destroy(&context);
+        log_error ("failed to allocate config for context");
+        dx_context_destroy (&context);
         return DISIR_STATUS_NO_MEMORY;
     }
 
@@ -68,24 +68,24 @@ dc_config_begin(dc_t **config)
 
 //! PUBLIC API
 enum disir_status
-dc_config_begin_supplied_schema(struct disir_schema *schema, dc_t **config)
+dc_config_begin_supplied_schema (struct disir_schema *schema, dc_t **config)
 {
     enum disir_status status;
 
     if (schema == NULL)
     {
-        log_debug("invoked with NULL schema pointer");
+        log_debug ("invoked with NULL schema pointer");
         return DISIR_STATUS_INVALID_ARGUMENT;
     }
 
-    status = dc_config_begin(config);
+    status = dc_config_begin (config);
     if (status != DISIR_STATUS_OK)
     {
         // Log already produced
         return status;
     }
 
-    status = dc_config_attach_schema(*config, schema);
+    status = dc_config_attach_schema (*config, schema);
     if (status != DISIR_STATUS_OK)
     {
         // Log already produced
@@ -98,18 +98,18 @@ dc_config_begin_supplied_schema(struct disir_schema *schema, dc_t **config)
 
 //! PUBLIC API
 enum disir_status
-dc_config_attach_schema(dc_t *config, struct disir_schema *schema)
+dc_config_attach_schema (dc_t *config, struct disir_schema *schema)
 {
     enum disir_status status;
 
     if (config == NULL || schema == NULL)
     {
         // LOGWARN
-        log_debug("invoked with NULL pointer(s) (config: %p \t schema: %p)", config, schema);
+        log_debug ("invoked with NULL pointer(s) (config: %p \t schema: %p)", config, schema);
         return DISIR_STATUS_INVALID_ARGUMENT;
     }
 
-    status = CONTEXT_TYPE_CHECK(config, DISIR_CONTEXT_CONFIG);
+    status = CONTEXT_TYPE_CHECK (config, DISIR_CONTEXT_CONFIG);
     if (status != DISIR_STATUS_OK)
     {
         // log produced by CONTEXT_TYPE_CHECK
@@ -119,7 +119,7 @@ dc_config_attach_schema(dc_t *config, struct disir_schema *schema)
     // Is there already a schema associated with this config?
     if (config->cx_config->cf_schema != NULL)
     {
-        dx_log_context(config, "schema already exists.");
+        dx_log_context (config, "schema already exists.");
         return DISIR_STATUS_EXISTS;
     }
 
@@ -132,11 +132,11 @@ dc_config_attach_schema(dc_t *config, struct disir_schema *schema)
 
 //! INTERNAL API
 struct disir_config *
-dx_config_create(dc_t *context)
+dx_config_create (dc_t *context)
 {
     struct disir_config *config;
 
-    config = calloc(1, sizeof(struct disir_config));
+    config = calloc (1, sizeof (struct disir_config));
     if (config ==  NULL)
         return config;
 
@@ -147,7 +147,7 @@ dx_config_create(dc_t *context)
 
 //! INTERNAL API
 enum disir_status
-dx_config_destroy(struct disir_config **config)
+dx_config_destroy (struct disir_config **config)
 {
     dc_t *context;
     if (config == NULL || *config == NULL)
@@ -159,10 +159,10 @@ dx_config_destroy(struct disir_config **config)
     if ((*config)->cf_documentation)
     {
         context = (*config)->cf_documentation->dd_context;
-        dc_destroy(&context);
+        dc_destroy (&context);
     }
 
-    free(*config);
+    free (*config);
     *config = NULL;
 
     return DISIR_STATUS_OK;
