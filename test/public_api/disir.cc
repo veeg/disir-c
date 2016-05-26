7535e688b0a19ba8fb2bf971629408cbc78f0a71
Vegard Sandengen <vegardsandengen@gmail.com>
2016-05-26 22:56:50 +0200

    test: add first simple disir instance test

---
+++ test/public_api/disir.cc
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+// PUBLIC API
+#include <disir/disir.h>
+
+#include "test_helper.h"
+
+
+
+class DisirInstanceTest : public testing::Test
+{
+    void SetUp()
+    {
+        status = DISIR_STATUS_OK;
+        disir = NULL;
+    }
+
+    void TearDown()
+    {
+    }
+
+public:
+    enum disir_status status;
+    struct disir *disir;
+};
+
+TEST_F (DisirInstanceTest, should_create_destroy_disir_instance)
+{
+    status = disir_instance_create (NULL, NULL, &disir);
+    ASSERT_EQ (status, DISIR_STATUS_OK);
+    ASSERT_TRUE (disir != NULL);
+
+    status = disir_instance_destroy (&disir);
+    ASSERT_EQ (status, DISIR_STATUS_OK);
+    ASSERT_TRUE (disir == NULL);
+}
+
