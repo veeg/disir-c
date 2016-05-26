8218293a0c785b79bcbe9ac2c4f6884c0fd1d815
Vegard Sandengen <vegardsandengen@gmail.com>
2016-05-26 22:57:06 +0200

    test: add disir_value stub test (no working test)

---
+++ test/internal_util/disir_value.cc
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <map>
+
+// PRIVATE API
+extern "C" {
+#include "disir_private.h"
+}
+
+#include "test_helper.h"
+
+class DisirValueTest : public testing::Test
+{
+    void SetUp()
+    {
+    }
+
+    void TearDown()
+    {
+    }
+};
+
+TEST_F (DisirValueTest, tmp)
+{
+}
