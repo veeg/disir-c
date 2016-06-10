43c83abb7624b8d2af3f1a49a7be98d40d226acb
Vegard Sandengen <vegardsandengen@gmail.com>
2016-06-10 23:58:54 +0200

    test: add initial context_section test

--- test/public_api/context_section.cc
+++ test/public_api/context_section.cc
@@ -3,46 +3,76 @@
 // PUBLIC API
 #include <disir/disir.h>
 
-// PRVIATE API
-extern "C" {
-#include "context_private.h"
-}
-
 #include "test_helper.h"
 
 
 // Test mold API with empty mold.
-class ContextSectionTest : public testing::Test
+class ContextSectionTest : public testing::DisirTestWrapper
 {
     void SetUp()
     {
-        context = dx_context_create (DISIR_CONTEXT_SECTION);
-        ASSERT_TRUE (context != NULL);
+        DisirLogCurrentTestEnter ();
+
+        context = NULL;
+        context_mold = NULL;
+        context_keyval = NULL;
+        context_section = NULL;
+
+        status = dc_mold_begin (&context_mold);
+        ASSERT_STATUS (DISIR_STATUS_OK, status);
+        status = dc_begin (context_mold, DISIR_CONTEXT_SECTION, &context_section);
+        ASSERT_STATUS (DISIR_STATUS_OK, status);
     }
 
     void TearDown()
     {
-
         if (context)
         {
-            dx_context_destroy (&context);
+            status = dc_destroy (&context);
+            EXPECT_STATUS (DISIR_STATUS_OK, status);
+        }
+        if (context_mold)
+        {
+            status = dc_destroy (&context_mold);
+            EXPECT_STATUS (DISIR_STATUS_OK, status);
+        }
+        if (context_keyval)
+        {
+            status = dc_destroy (&context_keyval);
+            EXPECT_STATUS (DISIR_STATUS_OK, status);
         }
 
+        DisirLogCurrentTestExit ();
     }
 
 public:
     enum disir_status status;
     struct disir_context *context;
+    struct disir_context *context_mold;
+    struct disir_context *context_section;
+    struct disir_context *context_keyval;
 };
 
 
 TEST_F (ContextSectionTest, context_type)
 {
-    ASSERT_EQ (dc_context_type (context), DISIR_CONTEXT_SECTION);
+    ASSERT_EQ (DISIR_CONTEXT_SECTION, dc_context_type (context_section));
 }
 
 TEST_F (ContextSectionTest, context_type_string)
 {
-    ASSERT_STREQ (dc_context_type_string (context), "SECTION");
+    ASSERT_STREQ ("SECTION", dc_context_type_string (context_section));
 }
 
+TEST_F (ContextSectionTest, finalizing_without_name_shall_fail)
+{
+    status = dc_finalize (&context_section);
+    EXPECT_STATUS (DISIR_STATUS_INVALID_CONTEXT, status);
+    EXPECT_STREQ ("Missing name component for section.", dc_context_error (context_section));
+}
+
+TEST_F (ContextSectionTest, set_name_on_mold)
+{
+    status = dc_set_name (context_section, "test_name", strlen ("test_name"));
+    EXPECT_STATUS (DISIR_STATUS_OK, status);
+}
