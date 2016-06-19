1eb1c2cce227e008fc748b4ce4f392d110d17790
Vegard Sandengen <vegardsandengen@gmail.com>
2016-06-19 17:40:32 +0200

    test: renamed context_keyval.cc - added more tests

--- test/public_api/context_mold_keyval.cc
+++ test/public_api/context_mold_keyval.cc
@@ -10,6 +10,7 @@
 // Test mold API with empty mold.
 class MoldKeyvalTest : public testing::DisirTestWrapper
 {
+protected:
     void SetUp()
     {
         DisirLogCurrentTestEnter ();
@@ -52,6 +53,22 @@ public:
 };
 
 
+class MoldKeyvalStringTest : public MoldKeyvalTest
+{
+    void SetUp ()
+    {
+        MoldKeyvalTest::SetUp ();
+
+        status = dc_set_value_type (context_keyval, DISIR_VALUE_TYPE_STRING);
+        ASSERT_STATUS (DISIR_STATUS_OK, status);
+
+        const char rname[] = "test_keyval_name";
+        status = dc_set_name (context_keyval, rname, strlen (rname));
+        ASSERT_STATUS (DISIR_STATUS_OK, status);
+    }
+};
+
+
 TEST_F (MoldKeyvalTest, context_type)
 {
     ASSERT_EQ (dc_context_type (context_keyval), DISIR_CONTEXT_KEYVAL);
@@ -89,4 +106,74 @@ TEST_F (MoldKeyvalTest, get_name)
     ASSERT_STREQ (rname, qname);
     ASSERT_EQ (strlen (rname), size);
 }
+
+TEST_F (MoldKeyvalTest, add_documentation)
+{
+    const char doc[] = "test doc string";
+
+    status = dc_add_documentation (context_keyval, doc, strlen (doc));
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+}
+
+TEST_F (MoldKeyvalTest, get_documentation)
+{
+    const char doc[] = "test doc string";
+    const char *qdoc;
+    int32_t size;
+
+    // setup
+    status = dc_add_documentation (context_keyval, doc, strlen (doc));
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+
+    status = dc_get_documentation (context_keyval, NULL, &qdoc, NULL);
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+    ASSERT_STREQ (doc, qdoc);
+
+    status = dc_get_documentation (context_keyval, NULL, &qdoc, &size);
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+    ASSERT_EQ (strlen (doc), size);
+}
+
+TEST_F (MoldKeyvalTest, set_type_string)
+{
+    status = dc_set_value_type (context_keyval, DISIR_VALUE_TYPE_STRING);
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+}
+
+TEST_F (MoldKeyvalTest, set_type_int)
+{
+    status = dc_set_value_type (context_keyval, DISIR_VALUE_TYPE_INTEGER);
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+}
+
+TEST_F (MoldKeyvalTest, set_type_float)
+{
+    status = dc_set_value_type (context_keyval, DISIR_VALUE_TYPE_FLOAT);
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+}
+
+TEST_F (MoldKeyvalTest, set_type_boolean)
+{
+    status = dc_set_value_type (context_keyval, DISIR_VALUE_TYPE_BOOLEAN);
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+}
+
+TEST_F (MoldKeyvalStringTest, add_string_default_shall_succeed)
+{
+    const char testval[] = "test string value";
+
+    status = dc_add_default_string (context_keyval, testval, strlen (testval), NULL);
+    ASSERT_STATUS (DISIR_STATUS_OK, status);
+}
+
+TEST_F (MoldKeyvalStringTest, add_int_default_shall_fail)
+{
+    status = dc_add_default_integer (context_keyval, 42, NULL);
+    // XXX TODO: This should probably return a more descriptive error?
+    ASSERT_STATUS (DISIR_STATUS_INVALID_ARGUMENT, status);
+
+    ASSERT_STREQ ("cannot set integer value on context whose value type is STRING",
+                  dc_context_error (context_keyval));
+}
+
 
