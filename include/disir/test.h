a309d29ba23593fae7e51c3e0fa7002111738b08
Vegard Sandengen <vegardsandengen@gmail.com>
2017-01-15 23:56:57 +0100

    refactor: move test plugin implementation into libdisir

---
+++ include/disir/test.h
@@ -0,0 +1,38 @@
+#ifndef _LIBDISIR_TEST_H
+#define _LIBDISIR_TEST_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif // _cplusplus
+
+enum disir_status dio_test_config_read (struct disir_instance *instance,
+                                        struct disir_plugin *plugin, const char *entry_id,
+                                        struct disir_mold *mold, struct disir_config **config);
+
+enum disir_status dio_test_config_entries (struct disir_instance *instance,
+                                           struct disir_plugin *plugin,
+                                           struct disir_entry **entries);
+
+enum disir_status dio_test_config_query (struct disir_instance *instance,
+                                         struct disir_plugin *plugin,
+                                         const char *entry_id);
+
+enum disir_status dio_test_mold_read (struct disir_instance *instance,
+                                      struct disir_plugin *plugin,
+                                      const char *entry_id,
+                                      struct disir_mold **mold);
+
+enum disir_status dio_test_mold_entries (struct disir_instance *instance,
+                                         struct disir_plugin *plugin,
+                                         struct disir_entry **entries);
+
+enum disir_status dio_test_mold_query (struct disir_instance *instance,
+                                       struct disir_plugin *plugin,
+                                       const char *entry_id);
+
+#ifdef __cplusplus
+}
+#endif // _cplusplus
+
+#endif // _LIBDISIR_TEST_H
+
