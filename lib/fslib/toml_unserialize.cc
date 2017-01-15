8494b40db329bb36b963a4a796dd037e10034452
Vegard Sandengen <vegardsandengen@gmail.com>
2017-01-15 19:48:33 +0100

    lib feat: Finish initial fslib toml implementation

--- lib/fslib/toml_unserialize.cc
+++ lib/fslib/toml_unserialize.cc
@@ -12,6 +12,52 @@
 #define ATTRIBUTE_KEY_DISIR_CONFIG_VERSION "@DISIR_CONFIG_VERSION"
 #define ATTRIBUTE_KEY_DISIR_CONFIG_VERSION_QUOTED "\"@DISIR_CONFIG_VERSION\""
 
+// Forward declare
+static enum disir_status
+dio_toml_unserialize_all (struct disir_instance *instance, const char *key,
+                          const toml::Value& value, struct disir_context *parent);
+
+
+// STATIC
+static enum disir_status
+dio_toml_parse_array (struct disir_instance *instance, const char *key,
+                      const toml::Value& array, struct disir_context *parent)
+{
+    enum disir_status status = DISIR_STATUS_OK;
+
+    // Iterate the array, man
+    for (const auto& entry : array.as<toml::Array> ())
+    {
+        status = dio_toml_unserialize_all (instance, key, entry, parent);
+        if (status != DISIR_STATUS_OK)
+        {
+            break;
+        }
+    }
+
+    return status;
+}
+
+// STATIC
+static enum disir_status
+dio_toml_parse_table (struct disir_instance *instance, const toml::Value& table,
+                      struct disir_context *parent)
+{
+    enum disir_status status = DISIR_STATUS_OK;
+
+    // Iterate the table, man
+    for (const auto& kv : table.as<toml::Table> ())
+    {
+        status = dio_toml_unserialize_all (instance, kv.first.c_str (), kv.second, parent);
+        if (status != DISIR_STATUS_OK)
+        {
+            break;
+        }
+    }
+
+    return status;
+}
+
 static enum disir_status
 dio_toml_unserialize_primitive (struct disir_instance *instance, const char *key,
                                 const toml::Value& value, struct disir_context *parent)
@@ -85,68 +131,91 @@ error:
 }
 
 static enum disir_status
-dio_toml_parse_table (struct disir_instance *instance, toml::Value& table,
-                      struct disir_context *parent)
+dio_toml_unserialize_table (struct disir_instance *instance, const char *key,
+                            const toml::Value& table, struct disir_context *parent)
+{
+    enum disir_status status;
+    struct disir_context *context;
+
+    status = dc_begin (parent, DISIR_CONTEXT_SECTION, &context);
+    if (status != DISIR_STATUS_OK)
+    {
+        disir_log_user (instance, "TOML: Cannot construct SECTION on parent: %s",
+                                  disir_status_string (status));
+        goto error;
+    }
+
+    status = dc_set_name (context, key, strlen (key));
+    if (status != DISIR_STATUS_OK)
+    {
+        // QUESTION: Handle the sitation?
+        disir_log_user (instance, "TOML: Name error for SECTION: %s",
+                                  disir_status_string (status));
+        goto error;
+    }
+
+    // Parse the table and append it to the newly created section
+    status = dio_toml_parse_table (instance, table, context);
+    if (status != DISIR_STATUS_OK)
+    {
+        disir_log_user (instance, "TOML: Sub-table failed convert: %s",
+                                  disir_status_string (status));
+        goto error;
+    }
+
+    return dc_finalize (&context);
+error:
+
+    dc_destroy (&context);
+    return status;
+}
+
+static enum disir_status
+dio_toml_unserialize_all (struct disir_instance *instance, const char *key,
+                          const toml::Value& value, struct disir_context *parent)
 {
     enum disir_status status = DISIR_STATUS_OK;
 
-    // Iterate the table, man
-    for (const auto& kv : table.as<toml::Table> ())
+    switch (value.type())
     {
-        switch (kv.second.type())
-        {
-        case toml::Value::NULL_TYPE:
-        {
-            disir_log_user (instance, "TOML: Key '%s' of NULL type. Cannot reasonably handle.");
-            status = DISIR_STATUS_INTERNAL_ERROR;
-        }
-        case toml::Value::BOOL_TYPE:
-        {
-            status = dio_toml_unserialize_primitive (instance, kv.first.c_str(), kv.second, parent);
-            break;
-        }
-        case toml::Value::INT_TYPE:
-        {
-            status = dio_toml_unserialize_primitive (instance, kv.first.c_str(), kv.second, parent);
-            break;
-        }
-        case toml::Value::DOUBLE_TYPE:
-        {
-            status = dio_toml_unserialize_primitive (instance, kv.first.c_str(), kv.second, parent);
-            break;
-        }
-        case toml::Value::STRING_TYPE:
-        {
-            status = dio_toml_unserialize_primitive (instance, kv.first.c_str(), kv.second, parent);
-            break;
-        }
-        case toml::Value::TABLE_TYPE:
-        case toml::Value::ARRAY_TYPE:
-        {
-            disir_log_user (instance, "TOML: Type not implemented: %s", kv.second.typeAsString());
-            // TODO: Just loop over this shizzle. Should probably call contents of this loop.
-            status = DISIR_STATUS_INTERNAL_ERROR;
-            break;
-        }
-        case toml::Value::TIME_TYPE:
-        {
-            disir_log_user (instance, "TOML: Time type not supported by Disir.");
-            // TODO: Revise error code.
-            status = DISIR_STATUS_INTERNAL_ERROR;
-            break;
-        }
-        default:
-        {
-            disir_log_user (instance, "TOML: Type not handled: %s", kv.second.typeAsString ());
-            status = DISIR_STATUS_INTERNAL_ERROR;
-            break;
-        }
-        }
-
-        if (status != DISIR_STATUS_OK)
-        {
-            break;
-        }
+    case toml::Value::NULL_TYPE:
+    {
+        disir_log_user (instance, "TOML: Key '%s' of NULL type. Cannot reasonably handle.");
+        status = DISIR_STATUS_INTERNAL_ERROR;
+        break;
+    }
+    case toml::Value::BOOL_TYPE:
+    case toml::Value::INT_TYPE:
+    case toml::Value::DOUBLE_TYPE:
+    case toml::Value::STRING_TYPE:
+    {
+        status = dio_toml_unserialize_primitive (instance, key, value, parent);
+        break;
+    }
+    case toml::Value::TABLE_TYPE:
+    {
+        // Create a new section under parent, and send that into parse_table (recursive)
+        status = dio_toml_unserialize_table (instance, key, value, parent);
+        break;
+    }
+    case toml::Value::ARRAY_TYPE:
+    {
+        status = dio_toml_parse_array (instance, key, value, parent);
+        break;
+    }
+    case toml::Value::TIME_TYPE:
+    {
+        disir_log_user (instance, "TOML: Time type not supported by Disir.");
+        // TODO: Revise error code.
+        status = DISIR_STATUS_INTERNAL_ERROR;
+        break;
+    }
+    default:
+    {
+        disir_log_user (instance, "TOML: Type not handled: %s", value.typeAsString ());
+        status = DISIR_STATUS_INTERNAL_ERROR;
+        break;
+    }
     }
 
     return status;
