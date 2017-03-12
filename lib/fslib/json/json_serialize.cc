89aed25d786f2f16629eca1f0cabf321de62e850
Vegard Sandengen <vegardsandengen@gmail.com>
2017-03-12 16:09:47 +0100

    feat json: finish initial (un)serialize api implementation
    This implementations only handles happy-path (un)serialization.

--- lib/fslib/json/json_serialize.cc
+++ lib/fslib/json/json_serialize.cc
@@ -8,10 +8,61 @@
 #include <disir/disir.h>
 #include <disir/fslib/json.h>
 
+
 //! FSLIB API
 enum disir_status
-dio_json_serialize_config (struct disir_config *config, FILE* output)
+dio_json_serialize_config (struct disir_instance *instance,
+                           struct disir_config *config, FILE* output)
 {
-    return DISIR_STATUS_INTERNAL_ERROR;
+    disir_log_user (instance, "TRACE ENTER dio_json_serialize_config");
+
+    try
+    {
+        enum dplugin_status pstatus;
+        boost::fdostream file(fileno(output));
+        dio::ConfigWriter writer (NULL);
+
+        pstatus = writer.marshal (config, file);
+        if (pstatus != DPLUGIN_STATUS_OK)
+        {
+            disir_log_user (instance, "JSON: serialize_config failed");
+            return DISIR_STATUS_INTERNAL_ERROR;
+        }
+    }
+    catch (std::exception& e)
+    {
+        return DISIR_STATUS_INTERNAL_ERROR;
+    }
+
+    return DISIR_STATUS_OK;
+}
+
+// FSLIB API
+enum disir_status
+dio_json_serialize_mold (struct disir_instance *instance,
+                         struct disir_mold *mold, FILE *output)
+{
+    disir_log_user (instance, "TRACE ENTER dio_json_serialize_mold");
+
+    try
+    {
+        enum dplugin_status pstatus;
+        boost::fdostream file(fileno(output));
+        dio::MoldWriter writer (instance);
+
+        pstatus = writer.marshal (mold, file);
+        if (pstatus != DPLUGIN_STATUS_OK)
+        {
+            disir_log_user (instance, "JSON: serialize_mold failed");
+            return DISIR_STATUS_INTERNAL_ERROR;
+        }
+    }
+    catch (std::exception& e)
+    {
+        return DISIR_STATUS_INTERNAL_ERROR;
+    }
+
+    return DISIR_STATUS_OK;
+
 }
 
--- lib/fslib/json/json_unserialize.cc
+++ lib/fslib/json/json_unserialize.cc
@@ -8,11 +8,61 @@
 #include <disir/disir.h>
 #include <disir/fslib/json.h>
 
+
 //! FSLIB API
 enum disir_status
 dio_json_unserialize_config (struct disir_instance *instance, FILE *input,
                              struct disir_mold *mold, struct disir_config **config)
 {
-    return DISIR_STATUS_INTERNAL_ERROR;
+    disir_log_user (instance, "TRACE ENTER dio_json_unserialize_config");
+    try
+    {
+        enum dplugin_status pstatus;
+        boost::fdistream file(fileno(input));
+        dio::ConfigReader reader (instance, mold);
+
+        pstatus = reader.unmarshal (config, file);
+        if (pstatus != DPLUGIN_STATUS_OK)
+        {
+            disir_log_user (instance, "JSON: unserialize_config failed");
+            return DISIR_STATUS_INTERNAL_ERROR;
+        }
+    }
+    catch (std::exception& e)
+    {
+        disir_log_user (instance, "JSON: fatal exception in unserialize_config");
+        return DISIR_STATUS_INTERNAL_ERROR;
+    }
+
+    return DISIR_STATUS_OK;
+}
+
+//! FSLIB API
+enum disir_status
+dio_json_unserialize_mold (struct disir_instance *instance,
+                           FILE *input, struct disir_mold **mold)
+{
+    disir_log_user (instance, "TRACE ENTER dio_json_unserialize_mold");
+
+    try
+    {
+        enum dplugin_status pstatus;
+        boost::fdistream file(fileno(input));
+        dio::MoldReader reader (instance);
+
+        pstatus = reader.unmarshal (file, mold);
+        if (pstatus != DPLUGIN_STATUS_OK)
+        {
+            disir_log_user (instance, "JSON: unserialize_mold failed");
+            return DISIR_STATUS_INTERNAL_ERROR;
+        }
+    }
+    catch (std::exception& e)
+    {
+        disir_log_user (instance, "JSON: fatal exception in unserialize_mold");
+        return DISIR_STATUS_INTERNAL_ERROR;
+    }
+
+    return DISIR_STATUS_OK;
 }
 
