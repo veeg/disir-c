e16859e99bdda9fdb9bd7939ab31f89673ba7d24
hoffa2 <helhof@online.no>
2017-07-28 13:26:45 +0200

    test lib: end-to-end export/import test

--- test/public_api/disir_archive/config_overrides/multiple_defaults_override.cc
+++ test/public_api/disir_archive/config_overrides/multiple_defaults_override.cc
@@ -111,3 +111,43 @@ multiple_defaults_override_1_2 (struct disir_mold *mold, struct disir_config **c
 error:
     return status;
 }
+
+enum disir_status
+multiple_defaults_1_2_differ_1_0 (struct disir_mold *mold, struct disir_config **config)
+{
+    enum disir_status status;
+    struct disir_version version;
+    struct disir_context *context_config;
+    struct disir_context *context_keyval;
+
+    version = {1,2};
+    status = disir_generate_config_from_mold (mold, &version, config);
+    if (status != DISIR_STATUS_OK)
+        goto error;
+
+    context_config = dc_config_getcontext (*config);
+
+    status = dc_query_resolve_context (context_config, "keyval_string", &context_keyval);
+    if (status != DISIR_STATUS_OK)
+        goto error;
+
+    status = dc_set_value_string (context_keyval, "keyval_differ", strlen ("keyval_differ"));
+    if (status != DISIR_STATUS_OK)
+        goto error;
+
+    dc_putcontext (&context_keyval);
+
+    status = dc_query_resolve_context (context_config, "integer", &context_keyval);
+    if (status != DISIR_STATUS_OK)
+        goto error;
+
+    status = dc_set_value_integer (context_keyval, 11);
+    if (status != DISIR_STATUS_OK)
+        goto error;
+
+    dc_putcontext (&context_keyval);
+
+    return status;
+error:
+    return status;
+}
